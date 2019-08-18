79a610c639ba450c6e57f29205d7725227975b17
Barbie <barbie@missbarbell.co.uk>
2019-08-18 14:17:21 +0000

    updated version

--- lib/Bot/BasicBot/Pluggable/Module/Crontab.pm
+++ lib/Bot/BasicBot/Pluggable/Module/Crontab.pm
@@ -3,7 +3,7 @@ package Bot::BasicBot::Pluggable::Module::Crontab;
 use warnings;
 use strict;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
  
 #----------------------------------------------------------------------------
 
